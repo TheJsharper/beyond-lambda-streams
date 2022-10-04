555e55dbe8c97f5aa800fe2ae70f24ed902419c1
Pedro Antonio Salomon Ocampos <pedro.salomon@gmail.com>
2022-10-04 23:08:01 +0200

    generic comparator

--- src/lambda/comparator/StudentGenericComparator.java
+++ src/lambda/comparator/StudentGenericComparator.java
@@ -1,5 +1,6 @@
 package lambda.comparator;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.Arrays;
@@ -50,5 +51,60 @@ public class StudentGenericComparator {
 		return Arrays.stream(o1.getMethods()).filter(m -> m.getName().equals(name)).collect(Collectors.toList()).get(0);
 	}
 
-}
+	@SuppressWarnings("unchecked")
+	public static <T> Comparator<T> getGenericComparatorLambda(Class<?> clazz, String fieldName, boolean orderByAsc) {
 
+		// get field from class
+		final Field field = getField(clazz, fieldName);
+		field.setAccessible(true);
+
+		// Lambda comparator
+		Comparator<T> orderByComparator = (f1, f2) -> {
+
+			Comparable<T> compA = null;
+			Comparable<T> compB = null;
+
+			// getting fields from object f1,f2
+			try {
+				compA = (Comparable<T>) field.get(f1);
+				compB = (Comparable<T>) field.get(f2);
+			} catch (IllegalAccessException e) {
+				e.printStackTrace();
+			}
+
+			// handling null comparators
+			if (compA == null)
+				return -1;
+			else if (compB == null)
+				return 1;
+
+			return compA.compareTo((T) compB);
+		};
+
+		// reverse comparator if order is descending
+		if (orderByComparator != null && !orderByAsc)
+			orderByComparator = orderByComparator.reversed();
+
+		return orderByComparator;
+	}
+
+	private static <T> Field getField(Class<T> clazz, String fieldName) {
+		Field field = null;
+		try {
+			field = clazz.getDeclaredField(fieldName);
+		} catch (java.lang.NoSuchFieldException e) {
+			e.printStackTrace();
+		}
+		return field;
+	}
+
+	/*
+	 * public static <T> Comparator<T> orderByComparatorFnc(){ Comparator<T>
+	 * orderByComparator = (f1, f2) -> { Comparable compA, compB; try { compA =
+	 * (Comparable) field.get(f1); compB = (Comparable) field.get(f2); } catch
+	 * (IllegalAccessException e) { throw new RuntimeException(e); } return
+	 * Object.compare(compA, compB, Comparator.naturalOrder()); }; return
+	 * orderByComparator; }
+	 */
+
+}
--- src/lambda/comparator/declarative/DeclarativeComparator.java
+++ src/lambda/comparator/declarative/DeclarativeComparator.java
@@ -1,19 +1,21 @@
 package lambda.comparator.declarative;
 
-import lambda.comparator.lambda.model.NaturalComparator;
+import lambda.comparator.StudentGenericComparator;
 import lambda.comparator.lambda.model.Student;
 import lambda.comparator.utils.Utils;
 
 public class DeclarativeComparator {
 
 	public static void main(String[] args) {
-		var c =  new NaturalComparator<Student>();//StudentGenericComparator.getImperativeStyleComparatorFirstName(DIRECTION.DESC,
-				//"getFirstName");
+
+		var c = StudentGenericComparator.getGenericComparatorLambda(Student.class, "age", true); // new
+																										// NaturalComparator<Student>();//StudentGenericComparator.getImperativeStyleComparatorFirstName(DIRECTION.DESC,
+		// "getFirstName");
 		var student = Utils.createStudentList();
 		student.sort(c);
 		student.stream().forEach(System.out::println);
 
-		//Arrays.stream(Student.class.getMethods()).forEach(System.out::println);
+		// Arrays.stream(Student.class.getMethods()).forEach(System.out::println);
 	}
 
 }
