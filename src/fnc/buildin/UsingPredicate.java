672883d3a767d5f86efa1ed6d82b6fd9f9d88713
Pedro Antonio Salomon Ocampos <pedro.salomon@gmail.com>
2022-10-16 20:41:04 +0200

    implemented rest of predicate with native parameters

--- src/fnc/buildin/UsingPredicate.java
+++ src/fnc/buildin/UsingPredicate.java
@@ -3,6 +3,9 @@ package fnc.buildin;
 import java.util.List;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
+import java.util.function.DoublePredicate;
+import java.util.function.IntPredicate;
+import java.util.function.LongPredicate;
 import java.util.function.Predicate;
 
 import lambda.comparator.lambda.model.Address;
@@ -11,38 +14,54 @@ import static lambda.comparator.utils.Utils.*;
 
 public class UsingPredicate {
 
-	static Predicate<Student> isUpper18YearOld = (Student s) -> s.getAge() >= 18;
-	static Predicate<Student> isFemale = (Student s) -> s.getGender() == "female";
+	private static Predicate<Student> isUpper18YearOld = (Student s) -> s.getAge() >= 18;
 
-	static BiConsumer<String, List<Address>> printterByNameAndAddresses = (String name,
+	private static Predicate<Student> isFemale = (Student s) -> s.getGender() == "female";
+
+	private static BiConsumer<String, List<Address>> printterByNameAndAddresses = (String name,
 			List<Address> address) -> System.out.println("Name: " + name + " Address:" + address);
 
-	static Consumer<Student> printterByUpper18YearOld = (Student s) -> {
+	private static Consumer<Student> printterByUpper18YearOld = (Student s) -> {
 		if (isUpper18YearOld.test(s)) {
 			printterByNameAndAddresses.accept(s.getFirstName() + " " + s.getLastName(), s.getAddresses());
 		}
 	};
-	static Consumer<Student> printterByFemaleStudents = (Student s) -> {
+	private static Consumer<Student> printterByFemaleStudents = (Student s) -> {
 		if (isFemale.test(s)) {
 			printterByNameAndAddresses.accept(s.getFirstName() + " " + s.getLastName(), s.getAddresses());
 		}
 	};
-	static Consumer<Student> printterByFemaleAndUpper18YearStudents = (Student s) -> {
+	private static Consumer<Student> printterByFemaleAndUpper18YearStudents = (Student s) -> {
 		if (isFemale.and(isUpper18YearOld).test(s)) {
 			printterByNameAndAddresses.accept(s.getFirstName() + " " + s.getLastName(), s.getAddresses());
 		}
 	};
 
+	private static IntPredicate isModule10 = (int value) -> value % 10 == 0;
+
+	private static DoublePredicate isValuePiByPlace3 = (double value) -> value == 3.142;
+
+	private static LongPredicate isMaxLong = (long value) -> value == Long.MAX_VALUE;
+
 	public static void main(String[] args) {
 
 		printBasicsPredicate();
 		var students = getMergedSimpleStudentAdresses();
+
 		System.out.println("-------------------- Print Student by Female and 18 years-----------------");
 		students.forEach(printterByFemaleAndUpper18YearStudents);
+
 		System.out.println("-------------------- Print Student by Female-----------------");
 		students.forEach(printterByFemaleStudents);
+
 		System.out.println("-------------------- Print Studentby 18 years-----------------");
 		students.forEach(printterByUpper18YearOld);
+
+		System.out.println("Is Mdoule 10 == 30 :" + isModule10.test(50));
+
+		System.out.println("Is PI 3.14 :" + isValuePiByPlace3.test(3.14));
+
+		System.out.println("Is Long.MIN_VALUE ==  Long.MAX_VALUE :" + isMaxLong.test(Long.MIN_VALUE));
 	}
 
 	private static void printBasicsPredicate() {
