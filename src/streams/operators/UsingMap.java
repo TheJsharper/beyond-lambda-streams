19c8aaac79228b2118b402246c6b8fd385db23ef
Pedro Antonio Salomon Ocampos <pedro.salomon@gmail.com>
2022-10-17 22:59:59 +0200

    implemented mapMultiToLong/Int

--- src/streams/operators/UsingMap.java
+++ src/streams/operators/UsingMap.java
@@ -1,6 +1,7 @@
 package streams.operators;
 
-import static lambda.comparator.utils.Utils.*;
+import static lambda.comparator.utils.Utils.getMergedSimpleStudentAdresses;
+import static lambda.comparator.utils.Utils.getSimpleStudents;
 
 import java.util.Collection;
 import java.util.List;
@@ -8,6 +9,8 @@ import java.util.Map;
 import java.util.Set;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
+import java.util.function.IntConsumer;
+import java.util.function.LongConsumer;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -19,9 +22,21 @@ public class UsingMap {
 	public static void main(String[] args) {
 		var students = getSimpleStudents();
 		print(getFirstNameUpperCase(students), "getFirstNameUpperCase");
+		
 		print(getSetOfLastNameUpperCase(students), "getSetOfLastNameUpperCase");
+		
 		print(getFirstNameLengthMap(students), "getFirstNameLengthMap");
+		
 		printAddresses(getAddressesUsingMapMulti(getMergedSimpleStudentAdresses().stream()), "AddressesUsingMapMulti");
+		
+		System.out.printf("AVG AGES Value: %s  %s\n", getAgeAverageUsingMapMultiToDouble(students.stream()).toString(),
+				"getAgeAverageUsingMapMultiToDouble".toUpperCase());
+		
+		System.out.printf("MAX AGE Value: %s  %s\n", getAgeMaxUsingMapMultiToLong(students.stream()).toString(),
+				"getAgeMaxUsingMapMultiToLong".toUpperCase());
+		
+		System.out.printf("MIN AGE Value: %s  %s\n", getAgeMinUsingMapMultiToInt(students.stream()).toString(),
+				"getAgeMinUsingMapMultiToInt".toUpperCase());
 
 	}
 
@@ -41,6 +56,25 @@ public class UsingMap {
 
 		return students.mapMulti((Student s, Consumer<List<Address>> consumer) -> consumer.accept(s.getAddresses()))
 				.flatMap(List::stream).collect(Collectors.toList());
+	}
+
+	private static Double getAgeAverageUsingMapMultiToDouble(Stream<Student> students) {
+		var value = students.mapMultiToDouble((s, DoubleConsumer) -> {
+			DoubleConsumer.accept(s.getAge());
+		}).average();
+		return value.isPresent() ? value.getAsDouble() : 0.0;
+	}
+	private static Long getAgeMaxUsingMapMultiToLong(Stream<Student> students) {
+		var value = students.mapMultiToLong((Student s, LongConsumer longConsumer) -> {
+			longConsumer.accept(s.getAge());
+		}).max();
+		return value.isPresent() ? value.getAsLong() : 0;
+	}
+	private static Integer getAgeMinUsingMapMultiToInt(Stream<Student> students) {
+		var value = students.mapMultiToInt((Student s, IntConsumer longConsumer) -> {
+			longConsumer.accept(s.getAge());
+		}).min();
+		return value.isPresent() ? value.getAsInt() : 0;
 	}
 
 	private static void print(Collection<String> names, String Label) {
