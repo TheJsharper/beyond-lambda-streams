6e8061fcb339781b32503643cee299cacb5343bc
Pedro Antonio Salomon Ocampos <pedro.salomon@gmail.com>
2022-10-19 15:07:02 +0200

    min max as reduce function

--- src/streams/operators/UsingReduce.java
+++ src/streams/operators/UsingReduce.java
@@ -16,8 +16,18 @@ public class UsingReduce {
 
 		System.out.printf("Result :%d from getSimpleSum\n", getSimpleSum(Arrays.asList(new Integer[] { 1, 2, 3 })));
 
+		var values = Arrays.asList(new Integer[] { 5, 7, 50, 4, 2, 1, -1, 100, 2 });
+
+		getMaxValue(values).ifPresentOrElse((Integer value) -> System.out.println("Max Value " + value),
+				() -> System.err.print("Not FOUND MAX Value"));
+
+		getMinValue(values).ifPresentOrElse((Integer value) -> System.out.println("Min Value " + value),
+				() -> System.err.print("Not FOUND Min Value"));
+
 		print(getFullNames(students), "getFullNames");
+
 		Optional<Student> youngest = getMaybeStudentYoungest(students);
+
 		Optional<Student> oldest = getMaybeStudentOldest(students);
 
 		youngest.ifPresentOrElse((Student student) -> System.out.println("Youngster Student " + student),
@@ -25,13 +35,23 @@ public class UsingReduce {
 
 		oldest.ifPresentOrElse((Student student) -> System.out.println("Oldest Student " + student),
 				() -> System.err.print("Student empty"));
+
 		System.out.println("The most older Student :" + getOldestStudent(students));
+
 		System.out.println("The most younger Student: " + getYoungstStudent(students));
 
 	}
 
 	private static int getSimpleSum(List<Integer> values) {
 		return values.stream().reduce(0, (Integer acc, Integer next) -> acc + next);
+	}
+
+	private static Optional<Integer> getMaxValue(List<Integer> values) {
+		return values.stream().reduce((Integer acc, Integer next) -> acc > next ? acc : next);
+	}
+
+	private static Optional<Integer> getMinValue(List<Integer> values) {
+		return values.stream().reduce((Integer acc, Integer next) -> acc > next ? next : acc);
 	}
 
 	private static Optional<Student> getMaybeStudentYoungest(List<Student> students) {
