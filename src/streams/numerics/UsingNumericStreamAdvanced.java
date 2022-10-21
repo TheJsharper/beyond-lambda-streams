a84bf824aa083348bbc110bf78a15bb7b65b71ac
Pedro Antonio Salomon Ocampos <pedro.salomon@gmail.com>
2022-10-21 15:16:11 +0200

    implementation UsingNumericStreamAdavnced

--- src/streams/numerics/UsingNumericStreamAdvanced.java
+++ src/streams/numerics/UsingNumericStreamAdvanced.java
@@ -6,10 +6,8 @@ import static lambda.comparator.utils.Utils.print;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
-
-
-
 import java.util.stream.IntStream;
+import java.util.stream.LongStream;
 
 import lambda.comparator.lambda.model.Student;
 
@@ -18,16 +16,18 @@ public class UsingNumericStreamAdvanced {
 	public static void main(String[] args) {
 
 		var students = createStudentListWithAddresses();
-		
+
 		var values = Arrays.asList(new Integer[] { 2, 3, 5, 6, 20, 50 });
-		
+
 		getSimpleBoxing().forEach((n) -> System.out.print(n + " ,"));
-		
+
 		System.out.println();
-		
+
 		System.out.println("Sum: " + getSinpleUnBoxing(values));
+
+		print(getFromIntStreamForceMergeStudentList(students), "getFromIntStreamForceMergeStudentList");
 		
-		print(getForceMergeStudentList(students), "getForceMergeStudentList");
+		print(getFromLongStreamForceMergeStudentList(students), "getFromLongStreamForceMergeStudentList");
 	}
 
 	private static List<Integer> getSimpleBoxing() {
@@ -43,9 +43,15 @@ public class UsingNumericStreamAdvanced {
 
 	}
 
-	private static List<Student> getForceMergeStudentList(List<Student> students) {
+	private static List<Student> getFromIntStreamForceMergeStudentList(List<Student> students) {
 
 		return IntStream.range(0, students.size()).mapToObj((int n) -> students.get(n)).collect(Collectors.toList());
+	}
+
+	private static List<Student> getFromLongStreamForceMergeStudentList(List<Student> students) {
+
+		return LongStream.range(0, students.size()).mapToObj((long n) -> students.get((int) n))
+				.collect(Collectors.toList());
 	}
 
 }
