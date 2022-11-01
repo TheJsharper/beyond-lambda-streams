9a89146b22de92599f4084a908e7fa6ad807de2b
Pedro Antonio Salomon Ocampos <pedro.salomon@gmail.com>
2022-11-01 23:08:42 +0100

    UsingLocalDate

--- src/features_since_java8/interfaces/dates/UsingLocalDate.java
+++ src/features_since_java8/interfaces/dates/UsingLocalDate.java
@@ -2,10 +2,13 @@ package features_since_java8.interfaces.dates;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.temporal.ChronoField;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalAdjusters;
 import java.time.temporal.TemporalField;
 import java.util.Arrays;
 import java.util.List;
@@ -22,6 +25,10 @@ public class UsingLocalDate {
 		printLocalDateIntances();
 
 		printLocalDateMethods();
+
+		printModifyingValueImmutableLocalDate();
+
+		printComparableDate();
 	}
 
 	private static void printAllnewClazzes() {
@@ -103,6 +110,68 @@ public class UsingLocalDate {
 			e.printStackTrace();
 		}
 		return value;
+
+	}
+
+	private static void printModifyingValueImmutableLocalDate() {
+
+		LocalDate now = LocalDate.now();
+
+		System.out.printf("Before Plus Weeks: %s  After Plus Weeks: %s\n ", now.get(ChronoField.ALIGNED_WEEK_OF_MONTH),
+				now.plusWeeks(1).get(ChronoField.ALIGNED_WEEK_OF_MONTH));
+
+		System.out.printf("Before Plus Years: %s  After Plus Years: %s\n ", now, now.plusYears(1));
+
+		System.out.printf("Before Minus Years: %s  After Minus Years: %s\n ", now, now.minusYears(1));
+
+		System.out.printf("Before Plus Days: %s  After Plus Days: %s\n ", now, now.plusDays(1));
+
+		System.out.printf("Before Minus Days: %s  After Minus Days: %s\n ", now, now.minusDays(1));
+
+		System.out.printf("Before Plus Months: %s  After Plus Months: %s\n ", now, now.plusMonths(1));
+
+		System.out.printf("Before Minus Months: %s  After Minus Months: %s\n ", now, now.minusMonths(1));
+
+		System.out.printf("Before set Years: %s  After set Years: %s\n ", now, now.withYear(1995));
+
+		System.out.printf("Before set Months: %s  After set Months: %s\n ", now, now.withMonth(5));
+
+		System.out.printf("Before set Day of Days: %s  After set Day of Year: %s\n ", now, now.withDayOfYear(15));
+
+		System.out.printf("Before with(ChronoField.YEAR, 2019): %s  After with(ChronoField.YEAR, 2019): %s\n ", now,
+				now.with(ChronoField.YEAR, 2019));
+
+		System.out.printf(
+				"Before with(TemporalAdjusters.firstDayOfMonth): %s  with(TemporalAdjusters.firstDayOfMonth): %s\n ",
+				now, now.with(TemporalAdjusters.firstDayOfMonth()));
+
+		System.out.printf(
+				"Before with(TemporalAdjusters.dayOfWeekInMonth(1, DayOfWeek.FRIDAY)): %s  After with(TemporalAdjusters.dayOfWeekInMonth(1, DayOfWeek.FRIDAY)): %s\n ",
+				now, now.with(TemporalAdjusters.dayOfWeekInMonth(1, DayOfWeek.FRIDAY)));
+
+		System.out.printf("now.isSupported(ChronoUnit.DAYS)? %s  \n ", now.isSupported(ChronoUnit.DAYS));
+
+	}
+
+	private static void printComparableDate() {
+
+		LocalDate now = LocalDate.now();
+
+		LocalDate beforeNow = LocalDate.of(2022, 10, 31);
+
+		LocalDate afternow = LocalDate.of(2022, 12, 15);
+
+		System.out.printf("Is this year LeafYear?: %s\n", now.isLeapYear());
+
+		System.out.printf("Is this year 2000/01/31 LeafYear?: %s\n", LocalDate.of(2000, 01, 31).isLeapYear());
+
+		System.out.printf("Is %s  After %s ?: %s\n ", now, afternow, now.isAfter(afternow));
+		
+		System.out.printf("Is %s  After %s ?: %s\n ", afternow, now, afternow.isAfter(now));
+		
+		System.out.printf("Is %s  Before %s ?: %s\n ", now, beforeNow, now.isAfter(beforeNow));
+		
+		System.out.printf("Is %s  Before %s ?: %s\n ", beforeNow, now, beforeNow.isAfter(now));
 
 	}
 
