d60f57e51c684048088a3fdfad5fac1f505be801
Pedro Antonio Salomon Ocampos <pedro.salomon@gmail.com>
2022-11-03 12:25:59 +0100

    Converting from java.util.Date to java.time.LocalDate and Time

--- src/module-info.java
+++ src/module-info.java
@@ -3,6 +3,7 @@ module Test {
 	requires com.fasterxml.jackson.databind;
 	requires java.desktop;
 	requires java.base;
+	requires java.sql;
 
 	exports lambda.comparator.utils;
 	exports lambda.comparator.declarative;
